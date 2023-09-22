78a3686d2284514c75f6a3024a8baf4a35c087ca
Jay D <jdavenz8@gmail.com>
2023-09-22 12:52:07 +1200

    Initial Commit

---
+++ Radio/RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioApp.swift
+//  Radio
+//
+//  Created by Jigar on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
