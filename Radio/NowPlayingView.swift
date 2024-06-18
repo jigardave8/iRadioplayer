855b9653172f97fc980a7914bb0bbf933187f432
Jay D <jdavenz8@gmail.com>
2024-06-18 14:03:05 +1200

    ui fix

--- Radio/NowPlayingView.swift
+++ Radio/NowPlayingView.swift
@@ -7,78 +7,91 @@
 import SwiftUI
 import MediaPlayer
 
-
 struct NowPlayingView: View {
     @ObservedObject var audioPlayerManager: AudioPlayerManager
     @State private var currentTime: TimeInterval = 0
     @State private var isSeeking = false
 
     var body: some View {
-        VStack(spacing: 20) {
-            if let currentSong = getCurrentSong() {
-                // Album Art
-                AlbumArtView(albumArt: currentSong.artwork?.image(at: CGSize(width: 200, height: 200)))
-                
-                // Song Information
-                VStack(spacing: 10) {
-                    MarqueeText(text: currentSong.title ?? "Unknown Title", rate: 0.04)
+        GeometryReader { geometry in
+            VStack(spacing: 20) {
+                if let currentSong = getCurrentSong() {
+                    // Album Art
+                    AlbumArtView(albumArt: currentSong.artwork?.image(at: CGSize(width: 200, height: 200)))
+                        .frame(width: geometry.size.width * 0.6, height: geometry.size.width * 0.6)
+                    
+                    // Song Information
+                    VStack(spacing: 10) {
+                        MarqueeText(text: currentSong.title ?? "Unknown Title", rate: 0.04)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                        
+                        MarqueeText(text: currentSong.artist ?? "Unknown Artist", rate: 0.04)
+                            .font(.subheadline)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                        
+                        MarqueeText(text: currentSong.albumTitle ?? "Unknown Album", rate: 0.04)
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                    }
+                    
+                    // Current Time and Duration
+                    HStack {
+                        Text("\(formattedTime(time: currentTime))")
+                            .foregroundColor(.black)
+                        Spacer()
+                        Text("\(formattedTime(time: audioPlayerManager.currentSongDuration))")
+                            .foregroundColor(.black)
+                    }
+                    .padding(.horizontal, 10)
+                    
+                    // Playback Slider
+                    Slider(value: Binding(
+                        get: { currentTime },
+                        set: { newTime in
+                            audioPlayerManager.seek(to: newTime)
+                            currentTime = newTime
+                        }
+                    ), in: 0...(audioPlayerManager.currentSongDuration ?? 1))
+                    .accentColor(.green)
+                    .padding(.horizontal)
+                    .onReceive(audioPlayerManager.currentTimePublisher) { time in
+                        if !isSeeking {
+                            currentTime = time
+                        }
+                    }
+                } else {
+                    // Placeholder when no song is selected
+                    Text("No song selected")
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal)
-                    
-                    MarqueeText(text: currentSong.artist ?? "Unknown Artist", rate: 0.04)
-                        .font(.subheadline)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal)
-                    
-                    MarqueeText(text: currentSong.albumTitle ?? "Unknown Album", rate: 0.04)
-                        .font(.footnote)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal)
+                        .padding()
+                        .background(
+                            LinearGradient(gradient: Gradient(colors: [Color.gray, Color.black]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        )
+                        .padding()
                 }
-                
-                // Playback Slider
-                Slider(value: Binding(
-                    get: { currentTime },
-                    set: { newTime in
-                        audioPlayerManager.seek(to: newTime)
-                        currentTime = newTime
-                    }
-                ), in: 0...(audioPlayerManager.currentSongDuration ?? 1))
-                .accentColor(.orange)
-                .padding(.horizontal)
-                .onReceive(audioPlayerManager.currentTimePublisher) { time in
-                    if !isSeeking {
-                        currentTime = time
-                    }
-                }
-            } else {
-                Text("No song selected")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(
-                        LinearGradient(gradient: Gradient(colors: [Color.gray, Color.black]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
-                    )
-                    .padding()
             }
-        }
-        .padding()
-        .background(
-            LinearGradient(gradient: Gradient(colors: [Color.orange, Color.pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .cornerRadius(10)
-                .shadow(radius: 5)
-        )
-        .edgesIgnoringSafeArea(.all)
-        .onAppear {
-            audioPlayerManager.updateCurrentTime()
+            .padding()
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color.orange, Color.pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+            )
+            .edgesIgnoringSafeArea(.all)
+            .onAppear {
+                audioPlayerManager.updateCurrentTime()
+            }
         }
     }
     
@@ -87,6 +100,13 @@ struct NowPlayingView: View {
             return nil
         }
         return LibraryViewModel.shared.songs[audioPlayerManager.currentIndex]
+    }
+    
+    private func formattedTime(time: Double) -> String {
+        let interval = Int(time)
+        let minutes = interval / 60
+        let seconds = interval % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
@@ -99,14 +119,12 @@ struct AlbumArtView: View {
                 Image(uiImage: albumArt)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
                     .cornerRadius(10)
                     .shadow(radius: 5)
             } else {
                 Image(systemName: "music.note")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
                     .background(Color.gray)
                     .cornerRadius(10)
                     .shadow(radius: 5)
